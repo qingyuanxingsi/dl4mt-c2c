081f3ede125eab7367ceb145ddf4092438f74215
qingyuanxingsi <qingyuanxingsi@163.com>
2017-02-15 14:02:48 +0800

    Update Result

--- preprocess/filter_password_records.py
+++ preprocess/filter_password_records.py
@@ -1,12 +1,12 @@
 # -*- coding:utf-8 -*-
 
-local_file = r'D:\data\leak_final_20161219\pipw_1.0\test\all_pi-pw.pw.tok'
-out_file = open(r'D:\data\leak_final_20161219\pipw_1.0\test\pwd_unique_test.txt', 'w')
+local_file = r'D:\data\leak_final\pipw_1.0\dev\all_pi-pw.pw.tok'
+out_file = open(r'D:\data\leak_final\pipw_1.0\dev\pwd_unique_dev.txt', 'w')
 
 pwd_set = set()
 pwd_cnt = 0
 for line in open(local_file):
-    pwd = line.strip()
+    pwd = line.rstrip()
     if pwd not in pwd_set:
         out_file.writelines(pwd+"\n")
         pwd_set.add(pwd)
